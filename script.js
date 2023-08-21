82e9681596430c0147e3d8f62d25b9a1886937b2
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-08-21 13:30:38 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-//your code here
- .name{ max-width: 800px;
-            margin: 2em auto;
-            padding: 2em;
-            background-color: white;
-            border-radius: 5px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-	  }
